42c6e18f5079fdab19d1f9cd5d2fc41d6fa7f3b3
Emilio Luna <emilioluna1997@gmail.com>
2021-04-01 16:13:58 -0700

    markdown updated

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
 const inquirer = require("inquirer");
 const markdown = require("./markdown")
-const fs = require("fs"); 
+const fs = require("fs"); const fs = require("fs");
+const path = require("path");
+
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What's your GitHub username?"
+  },{
+    type: "input",
+    name: "email",
+    message: "What's your email?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What's your project's name?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Descripe your project."
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Do any dependencies need to be ran isntalled before running application?",
+    default: "npm i"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be ran to run tests?",
+    default: "npm test"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  }
+];
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+  inquirer.prompt(questions)
+  .then((inquirerResponses) => {
+    console.log("Generating README...");
+    writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+  })
+}
+
+init();
